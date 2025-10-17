fd74185fe63dd67e3930144b583e1e82a02572fd
Axel Vargas Rodriguez <axelvr91@gmail.com>
2025-10-16 23:20:50 -0600

    Test01

---
+++ SecondProject/Test01.py
@@ -0,0 +1,13 @@
+#01
+print("Resultado de la operacion01:")
+print(117+986)
+print(199-981)
+
+#02
+print("Resultado de la operacion02:")
+print(3+4+5-6)
+
+#03
+print("Resultado de la operacion03:")
+print(3+4)
+print(3 + 4)
