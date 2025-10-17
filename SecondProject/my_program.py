b6c568455b649c56c0d37c7b2c0fc634a746417c
Axel Vargas Rodriguez <axelvr91@gmail.com>
2025-10-16 23:13:28 -0600

    Operadores matematicos

--- SecondProject/my_program.py
+++ SecondProject/my_program.py
@@ -1,1 +1,21 @@
-print("aguita")
+
+#suma
+print("Suma:",2+2)
+
+#resta
+print("Resta:",4-2)
+
+#multiplicacion
+print("Multiplicacion:",4*2)
+
+#division
+print("Division:",8/2)
+
+#potencia
+print("Potencia:",5**2) 
+
+#modulo
+print("Modulo:",8%2)
+
+#division entera
+print("Division entera:",5//2)
