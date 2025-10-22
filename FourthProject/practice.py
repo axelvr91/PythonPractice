24cb9a02b7752423ff78547a36a1016762191a7a
Axel Vargas Rodriguez <axelvr91@gmail.com>
2025-10-21 20:43:25 -0600

    cap 35

---
+++ FourthProject/practice.py
@@ -0,0 +1,25 @@
+#permmite agregar un elemento al final de una lista
+workdays=["monday","tuesday","wednesday","thursday","friday"]
+weekend=["saturday","sunday"]
+seconds=[1.23,1.45,1.02]   
+current=1.11
+
+#esto agrega un elemento al final de una lista
+list.append(seconds,current)
+
+#esto elimina la primera aparicion de un elemento en una lista
+list.remove(seconds,1.45)
+
+#esto permite obtener un objeto en especifico de la lista
+first=seconds[0]
+
+#esto permite obtener un rango de objetos en una lista
+some_days=workdays[1:4]
+some_days2=workdays[3:]
+
+#indexado negativo, esto permite obtener elementos desde el final de la lista
+last_day=workdays[-1]
+
+
+
+
